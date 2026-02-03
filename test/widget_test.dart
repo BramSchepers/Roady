de7c5aa16c8e80d0f681d749e9fbaefd6bd5eb8f
BramSchepers <bram.schepers@outlook.com>
2026-02-02 21:19:16 -0600

    B1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:roady/auth/auth_state.dart';
 import 'package:roady/main.dart';
 
 void main() {
   testWidgets('App starts and shows Roady', (WidgetTester tester) async {
-    await tester.pumpWidget(const RoadyApp());
+    await tester.pumpWidget(RoadyApp(authState: AuthState()));
     expect(find.text('Roady', findRichText: true), findsOneWidget);
   });
 }
